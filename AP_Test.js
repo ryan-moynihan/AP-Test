c30690ab8251845362fa22b9424fd1d0f7736409
Ryan Q Moynihan <rm73819@srcs.loc>
2017-04-03 09:29:25 -0700

    made deal function

--- AP_Test.js
+++ AP_Test.js
@@ -1,8 +1,13 @@
-var J = 10
-var Q = 10
-var K = 10
+var J = 10;
+var Q = 10;
+var K = 10;
 var deck = [1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,J,J,J,J,Q,Q,Q,Q,K,K,K,K]
-function deal{
-    var card = Math.floor(Math.random * 53)
+var player = [];
 
+function dealCard(handsize) {
+    for (var i = 0; i < handsize; i++){
+        var card = Math.floor(Math.random * (deck.length + 1));
+        var deal = deck.splice(deck.indexOf(card), 1)[0];
+        player.push(deal);
+    }
 }
