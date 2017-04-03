9e4fcbcab123ce872d73c3e588f87e378715d0b1
Ryan Q Moynihan <rm73819@srcs.loc>
2017-04-03 08:30:49 -0700

    Made deck and set face cards to 10

---
+++ AP_Test.js
@@ -0,0 +1,8 @@
+var J = 10
+var Q = 10
+var K = 10
+var deck = [1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,J,J,J,J,Q,Q,Q,Q,K,K,K,K]
+function deal{
+    var card = Math.floor(Math.random * 53)
+
+}
