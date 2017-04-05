927d280968588205860af862a14d6b988885a086
Ryan Q Moynihan <rm73819@srcs.loc>
2017-04-05 08:15:49 -0700

    Changed deck added hit function

--- AP_Test.js
+++ AP_Test.js
@@ -1,7 +1,7 @@
 var J = 10;
 var Q = 10;
 var K = 10;
-var deck = [1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,J,J,J,J,Q,Q,Q,Q,K,K,K,K]
+var deck = [A,A,A,A,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,J,J,J,J,Q,Q,Q,Q,K,K,K,K]
 var player = [];
 
 function dealCard(handsize) {
@@ -10,4 +10,8 @@ function dealCard(handsize) {
         var deal = deck.splice(deck.indexOf(card), 1)[0];
         player.push(deal);
     }
+}
+
+function hit(){
+    dealCard(1)
 }
