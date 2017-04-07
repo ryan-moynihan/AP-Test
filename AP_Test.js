d31be00c110b87b65120290b645d4987bfc5da6e
Matthew Gess <torresvilla79@gmail.com>
2017-04-07 08:30:21 -0700

    trying to make the dealer

--- AP_Test.js
+++ AP_Test.js
@@ -4,12 +4,14 @@ var K = 10;
 var A = 11;
 var deck = [A, A, A, A, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, J, J, J, J, Q, Q, Q, Q, K, K, K, K]
 var player1 = [];
+var dealer = [];
 
 function dealCard(handsize) {
     for (var i = 0; i < handsize; i++) {
         var card = Math.floor(Math.random * (deck.length + 1));
         var deal = deck.splice(deck.indexOf(card), 1)[0];
         player1.push(deal);
+        dealer.push(deal);
     }
 }
 
@@ -34,4 +36,8 @@ function acesOrBust {
     if (player1 > 21 && player1.indexOf(A) >= 0) {
         A = 1
     }
+}
+
+function Dealer {
+    return
 }
