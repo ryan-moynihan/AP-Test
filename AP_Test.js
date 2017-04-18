537e920731730cf0a723ffba7bc4881791ac83d8
Matthew Gess <torresvilla79@gmail.com>
2017-04-17 22:53:45 -0700

    Fixed the deal function, made it so that all our funtctions also have an input slot for if it is the player or dealer, fixed the hit function, fixed the CheckForBust function, made an endTurn function that deals cards to the dealer and makes the dealer hit until he is busted or over  17

--- AP_Test.js
+++ AP_Test.js
@@ -2,48 +2,72 @@ var J = 10;
 var Q = 10;
 var K = 10;
 var A = 11;
-var deck = [A, A, A, A, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, J, J, J, J, Q, Q, Q, Q, K, K, K, K]
+var deckOrig = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
+var deck = [];
 var player1 = [];
 var dealer = [];
 
-function dealCard(handsize) {
+function dealCard(handsize, player) {
+    var card;
+    var deal;
     for (var i = 0; i < handsize; i++) {
-        var card = Math.floor(Math.random() * (deck.length + 1))
-        var deal = deck.splice(deck.indexOf(card), 1)[0];
-        player1.push(deal);
-        dealer.push(deal);
+        card = Math.floor(Math.random() * (deck.length));
+        console.log(i);
+        console.log(card);
+        deal = deck[card];
+        deck.splice(card, 1);
+        console.log(i);
+        console.log(deal);
+        player.push(deal);
     }
 }
 
-function hit() {
-    dealCard(1);
-    bust();
+function newGame() {
+    deck = deckOrig;
+}
+
+function hit(player) {
+    dealCard(1, player);
+    return checkForBust(player);
 }
 
 function add(a, b) {
     return a + b;
 }
 
-function bust() {
-    var sum = player1.reduce(add, 0);
-    if (player1 > 21) {
-        return sum
+function checkForBust(player) {
+    var sum = player.reduce(add, 0);
+    if (sum > 21) {
+        return "Total: " + sum + " BUSTED!"
     }
-    return "BUST"
+    else {
+        return "Total: " + sum
+    }
+}
+
+function endTurn() {
+    var done = false;
+    var sum;
+    dealCard(2, dealer);
+    do {
+        if (dealer.reduce(add, 0) < 17) {
+            hit(dealer);
+        }
+        sum = dealer.reduce(add, 0);
+        if (sum >= 17) {
+            done = true;
+            return checkForBust(dealer);
+        }
+    }
+    while (done == false)
 }
 
 function acesOrBust {
-    if (player1 > 21 && player1.indexOf(A) >= 0) {
+    if (player1.reduce(add, 0) > 21 && player1.indexOf(A) >= 0) {
         A = 1
     }
-}
-
-function Dealer {
-    if (dealer < 17) {
-        hit(1);
-    }
-    else if (dealer = 17 && dealer.indexOf(A) == 1) {
-        hit(1);
+    if (dealer.reduce(add, 0) > 21 && dealer.indexOf(A) >= 0) {
+        A =
     }
 }
 
